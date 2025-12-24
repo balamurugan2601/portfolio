b11a4f54d76bf6c1f9354aa6916c0c2ebac8a49d
Portfolio Developer <portfolio@example.com>
2025-12-25 01:15:52 +0530

    Portfolio updated

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -99,7 +99,7 @@ export const portfolioData: PortfolioData = {
             year: "2025",
             tags: ["Figma", "Affinity", "Photoshop", "Illustrator"],
             tagsLabel: "Tools",
-            link: "https://example.com",
+            link: "https://www.figma.com/design/mm6Ei7NdDbTBrJ3yi24apP/Balamurugan-Sundarraj-s-team-library?node-id=1812-38427&t=wKHmVUMMO139v9HK-1",
             gallery: [
                 { url: "https://ik.imagekit.io/shyambala/devfest/Main%20Banner%20-%20entry%2012x4.png", description: "Main Dashboard view ensuring high level metrics are visible immediately." },
                 { url: "https://ik.imagekit.io/shyambala/devfest/id.png?updatedAt=1766569862034", description: "Shopping cart interaction flow." },
